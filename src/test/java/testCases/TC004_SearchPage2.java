f42b75d75c9ef0b41560b1f179360c8312e186a8
lksajna <lksajna@gmail.com>
2024-02-21 10:51:08 +0100

    Correcting the text for printing

--- src/test/java/testCases/TC004_SearchPage2.java
+++ src/test/java/testCases/TC004_SearchPage2.java
@@ -4,7 +4,7 @@ public class TC004_SearchPage2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Registration page");
+		System.out.println("Search page");//Searching for items
 
 	}
 
